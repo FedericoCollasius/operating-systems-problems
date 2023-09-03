68541508ac15096c4ecee29f8fdb262f1ce63be0
Federico Collasius <fede.collasius@gmail.com>
2023-09-03 18:39:02 -0300

    Mi solucion del ejercicio 3 de la practica

--- Guia 1/Parte 2 - IPC/ipc_tests.c
+++ Guia 1/Parte 2 - IPC/ipc_tests.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h> // wait()
 #include <signal.h> // signals
 
+// Ver asignacion de indice que hace pipe a los fd.
 void test1(){
     int fd[2]; 
     printf("Soy fd[0] antes del pipe: %d\n", fd[0]);
@@ -19,6 +20,7 @@ void test1(){
     printf("Soy fd2[1] despues del pipe: %d\n", fd2[1]);
 }
 
+// Idem pero con los hijos.
 void test2(){
     int fd[2];
     pipe(fd); 
@@ -35,9 +37,65 @@ void test2(){
     }
 }
 
+// Esto es para ver como el read pisa el valor que habia en el buffer. 
+void test3(){
+    int fd[2][2];
+    pipe(fd[0]);
+    pipe(fd[1]);
+    pid_t pid; 
+    if((pid = fork()) == 0){
+        printf("Soy el primer hijo\n");
+        printf("Cierro los pipes que no voy a usar\n");
+        close(fd[0][0]);
+        close(fd[1][0]);
+        close(fd[1][1]);
+        int a = 1; 
+        printf("Escribo en el pipe 0\n");
+        write(fd[0][1], &a, sizeof(int));
+        printf("Cierro el pipe 0\n");
+        close(fd[0][1]);
+        printf("Termino el primer hijo\n");
+        exit(0);
+    } else {
+        wait(NULL);
+        if((pid = fork()) == 0){
+            printf("Soy el segundo hijo\n");
+            printf("Cierro los pipes que no voy a usar\n");
+            close(fd[0][0]);
+            close(fd[0][1]);
+            close(fd[1][0]);
+            int b = 2; 
+            printf("Escribo en el pipe 1\n");
+            write(fd[1][1], &b, sizeof(int));
+            printf("Cierro el pipe 1\n");
+            close(fd[1][1]);
+            printf("Termino el segundo hijo\n");
+            exit(0);
+        } else {
+            wait(NULL);
+            printf("Soy el padre\n");
+            printf("Cierro los pipes que no voy a usar\n");
+            close(fd[0][1]);
+            close(fd[1][1]);
+            int a;
+            int res; 
+            for(int i = 0; i < 2; i++){
+                printf("Leo del pipe %d\n", i);
+                read(fd[i][0], &a, sizeof(int));
+                res = res + a;
+                close(fd[i][0]);
+            }
+            printf("El resultado es: %d\n", res);
+            printf("Termino el padre\n");
+        }
+    }
+}
+
+
 
 int main(){
     // test1();
-    test2();
+    // test2();
+    test3();
     return 0; 
 }
--- Practicas/Practica-IPC/Ejercicio3/ejercicio_3.cpp
+++ Practicas/Practica-IPC/Ejercicio3/ejercicio_3.cpp
@@ -24,36 +24,92 @@ long contarPares(long minimo, long maximo) {
   return cantidad;
 }
 
+void hijo(int fd[]){
+  long minimo; 
+  std::cout << "Leo el minimo" << std::endl;
+  read(fd[0], &minimo, sizeof(minimo)); 
+  long maximo; 
+  std::cout << "Leo el maximo" << std::endl;
+  read(fd[0], &maximo, sizeof(maximo));
+  std::cout << "Cuento la cantidad de pares entre " << minimo << " y " << maximo << std::endl;
+  long cantidad = contarPares(minimo, maximo);
+  std::cout << "Escribo la cantidad de pares: " << cantidad << std::endl;
+  write(fd[1], &cantidad, sizeof(cantidad));
+  std::cout << "Cierro los pipes" << std::endl;
+  close(fd[0]); 
+  close(fd[1]);
+  std::cout << "Soy el hijo " << getpid() << " y termine el calculo" << std::endl;
+}
+
 int main(int argc, char const* argv[]) {
   // Contar el tiempo de ejecución del programa
   auto start = chrono::steady_clock::now();
 
   // Parseamos los parámetros
   if (argc != 2) {
-    cout << "Debe ejecutar con la cantidad de procesos N como parámetro. "
-            "Ejemplo:"
-         << endl;
-    cout << argv[0] << " N" << endl;
+    std::cout << "Debe ejecutar con la cantidad de procesos N como parámetro. Ejemplo:" << std::endl;
+    std::cout << argv[0] << " N" << std::endl;
     return 1;
   }
   procesos = atoi(argv[1]);
 
   // Creamos los N pipes
+  int fds[procesos][2]; 
+  for(int i = 0; i < procesos; i++){
+    pipe(fds[i]);
+  }
 
   // Creamos los N hijos
+  pid_t pid;
+  int i = 0; 
+  for(int i = 0; i < procesos; i++){
+    if((pid = fork()) == 0){
+      std::cout << "Soy el hijo " << getpid() << std::endl;
+      hijo(fds[i]);
+      std::cout << "Soy el hijo " << getpid() << " y termine" << std::endl;
+      exit(0);
+    }
+  }
 
-  // Calculamos e informamos los intervalos
+  // Calcular e informar rangos
+  std::cout << "Soy el padre y voy a calcular los rangos" << std::endl;
+  long cantidad = ((RANGO_MAX - RANGO_MIN) + (procesos - 1)) / procesos;
+  long inicio_rango = RANGO_MIN;
+  for (int i = 0; i < procesos; ++i) {
+    std::cout << "Calculo el rango del hijo " << i << std::endl;
+    long fin_rango = min(inicio_rango + cantidad, RANGO_MAX);
+    std::cout << "Escribo el rango del hijo " << i << ": " << inicio_rango << " - " << fin_rango << std::endl;
+    write(fds[i][1], &inicio_rango, sizeof(inicio_rango));
+    write(fds[i][1], &fin_rango, sizeof(fin_rango));
+    std::cout << "Cierro el pipe de escritura del hijo " << i << std::endl;
+    close(fds[i][1]);
+    inicio_rango += cantidad;
+  }
+
+  // Esperamos a los hijos
+  std::cout << "Soy el padre y voy a esperar a los hijos" << std::endl;
+  for(int i = 0; i < procesos; i++){
+    wait(NULL);
+  }
 
   // Recolectamos resultados
+  std::cout << "Soy el padre y voy a calcular el resultado" << std::endl;
   long resultado = 0;
+  long buffer; 
+  for(int i = 0; i < procesos; i++){
+    std::cout << "Leo el resultado del hijo " << i << std::endl;
+    read(fds[i][0], &buffer, sizeof(buffer));
+    std::cout << "El resultado del hijo " << i << " es " << buffer << std::endl;
+    resultado = resultado + buffer;
+    std::cout << "Cierro el pipe de lectura del hijo " << i << std::endl;
+    close(fds[i][0]);
+  }
 
   // Imprimimos resultado total
-  cout << "Resultado total: " << resultado << endl;
+  std::cout << "Resultado total: " << resultado << std::endl;
 
   auto end = chrono::steady_clock::now();
-  cout << "Tiempo total (ms): "
-       << chrono::duration_cast<chrono::milliseconds>(end - start).count()
-       << " ms" << endl;
+  std::cout << "Tiempo total (ms): " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << std::endl;
 
   return 0;
 }
