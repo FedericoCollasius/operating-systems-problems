c239ce03d026b1c46ed4e07ec4f4c950f5e332ca
Federico Collasius <fede.collasius@gmail.com>
2023-09-04 13:26:57 -0300

    Taller IPC Ej 2

--- Practicas/Practica-IPC/Ejercicio3/ejercicio_3.cpp
+++ Practicas/Practica-IPC/Ejercicio3/ejercicio_3.cpp
@@ -26,19 +26,13 @@ long contarPares(long minimo, long maximo) {
 
 void hijo(int fd[]){
   long minimo; 
-  std::cout << "Leo el minimo" << std::endl;
   read(fd[0], &minimo, sizeof(minimo)); 
   long maximo; 
-  std::cout << "Leo el maximo" << std::endl;
   read(fd[0], &maximo, sizeof(maximo));
-  std::cout << "Cuento la cantidad de pares entre " << minimo << " y " << maximo << std::endl;
   long cantidad = contarPares(minimo, maximo);
-  std::cout << "Escribo la cantidad de pares: " << cantidad << std::endl;
   write(fd[1], &cantidad, sizeof(cantidad));
-  std::cout << "Cierro los pipes" << std::endl;
   close(fd[0]); 
   close(fd[1]);
-  std::cout << "Soy el hijo " << getpid() << " y termine el calculo" << std::endl;
 }
 
 int main(int argc, char const* argv[]) {
@@ -64,44 +58,33 @@ int main(int argc, char const* argv[]) {
   int i = 0; 
   for(int i = 0; i < procesos; i++){
     if((pid = fork()) == 0){
-      std::cout << "Soy el hijo " << getpid() << std::endl;
       hijo(fds[i]);
-      std::cout << "Soy el hijo " << getpid() << " y termine" << std::endl;
       exit(0);
     }
   }
 
   // Calcular e informar rangos
-  std::cout << "Soy el padre y voy a calcular los rangos" << std::endl;
   long cantidad = ((RANGO_MAX - RANGO_MIN) + (procesos - 1)) / procesos;
   long inicio_rango = RANGO_MIN;
   for (int i = 0; i < procesos; ++i) {
-    std::cout << "Calculo el rango del hijo " << i << std::endl;
     long fin_rango = min(inicio_rango + cantidad, RANGO_MAX);
-    std::cout << "Escribo el rango del hijo " << i << ": " << inicio_rango << " - " << fin_rango << std::endl;
     write(fds[i][1], &inicio_rango, sizeof(inicio_rango));
     write(fds[i][1], &fin_rango, sizeof(fin_rango));
-    std::cout << "Cierro el pipe de escritura del hijo " << i << std::endl;
     close(fds[i][1]);
     inicio_rango += cantidad;
   }
 
   // Esperamos a los hijos
-  std::cout << "Soy el padre y voy a esperar a los hijos" << std::endl;
   for(int i = 0; i < procesos; i++){
     wait(NULL);
   }
 
   // Recolectamos resultados
-  std::cout << "Soy el padre y voy a calcular el resultado" << std::endl;
   long resultado = 0;
   long buffer; 
   for(int i = 0; i < procesos; i++){
-    std::cout << "Leo el resultado del hijo " << i << std::endl;
     read(fds[i][0], &buffer, sizeof(buffer));
-    std::cout << "El resultado del hijo " << i << " es " << buffer << std::endl;
     resultado = resultado + buffer;
-    std::cout << "Cierro el pipe de lectura del hijo " << i << std::endl;
     close(fds[i][0]);
   }
 
--- Talleres/Taller-IPC/pipes/Ejercicio2/anillo_alu.c
+++ Talleres/Taller-IPC/pipes/Ejercicio2/anillo_alu.c
@@ -4,27 +4,117 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
-#include "constants.h"
 
-int generate_random_number(){
-	return (rand() % 50);
-}
+#define PREV(i, n) ((i+n-1) % n)
+#define TERMINATE_SIGNAL -1
 
 int main(int argc, char **argv)
-{	
-	//Funcion para cargar nueva semilla para el numero aleatorio
-	srand(time(NULL));
+{   
+    int leer, escribir, pid, child2Wait, n;
+    int buffer;
 
-	int status, pid, n, start, buffer;
-	n = atoi(argv[1]);
-	buffer = atoi(argv[2]);
-	start = atoi(argv[3]);
+    if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
+    
+    /* Parsing de los argumentos */
+    n = atoi(argv[1]);
+    buffer = atoi(argv[2]);
+    int start = atoi(argv[3]);
+    printf("Se crearán %i procesos, se enviará el número %i desde proceso %i \n", n, buffer, start);
+    
+    /* Inicializar semilla aleatoria */
+    srand(time(NULL));
+  
+    /* El padre crea n+1 pipes (el último fuera del anillo)*/
+    int ring_pipes[n][2];
+    for(int j = 0; j < n; j++) 
+        if (pipe(ring_pipes[j]) < 0){ perror("Error al crear el pipe\n"); exit(0);}
 
-	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
-    
-  	/* COMPLETAR */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer, start);
-    
-    /* COMPLETAR */
-    
+    /* El padre crea n hijos que conocen todos los pipes */
+    int i;
+    for(i = 0; i < n; i++) 
+    {
+        if((pid = fork()) == 0 ) {
+        	if(i == start) child2Wait = pid;
+			break;
+		}
+    }
+
+    /* Si soy el hijo que empieza, escribo el numero inicial */
+    if (i == start)
+        write(ring_pipes[i][1], &buffer, sizeof(int));
+
+    /* Soy un hijo */
+    if(pid == 0)
+    {       
+        int secret_num = 0;
+
+        if (i == start){
+            secret_num = rand() % 20;
+            printf("Soy el proceso %d y mi número secreto es %d\n", i, secret_num);
+			for(int j = 0; j < n; j++){
+				if(j != PREV(start, n)){
+					close(ring_pipes[j][0]);
+				}
+			}
+        } else {
+			for(int j = 0; j< n; j++){
+        		if (j != i)
+            		close(ring_pipes[j][1]);
+        		if (j != PREV(i,n))     
+            		close(ring_pipes[j][0]);                 
+    		}			
+		}
+        
+        do 
+        {
+            read(ring_pipes[PREV(i,n)][0], &buffer, sizeof(int));
+            
+            if(buffer == TERMINATE_SIGNAL)
+                break;
+                
+            printf("Soy el hijo %d, con numero %d\n", i, buffer);
+            
+            if (i == start && buffer >= secret_num)
+            {
+				printf("Soy el hijo que empieza la comunicacion (%d) y superamos al numero secreto\n", i);
+				write(ring_pipes[start][1], &buffer, sizeof(int));
+                int terminate = TERMINATE_SIGNAL;
+				printf("Mando la señal de terminacion a los hijos\n");
+                for(int j=0; j < n; j++){
+					if (j != i)
+                    	write(ring_pipes[PREV(j, n)][1], &terminate, sizeof(int));
+						close(ring_pipes[PREV(j, n)][1]);
+				}
+                break;
+            }
+            
+            buffer++;
+            
+            write(ring_pipes[i][1], &buffer, sizeof(int));
+        } while (1);
+        
+		close(ring_pipes[PREV(i, n)][0]);
+		close(ring_pipes[i][1]);
+		printf("Soy el hijo %d y termine.\n", i);
+        exit(0);
+    }
+
+    /* El padre cierra pipes, espera al ultimo hijo y lee el ultimo valor*/
+    else if(pid > 0)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            close(ring_pipes[j][1]); 
+            if(j != start)
+                close(ring_pipes[j][0]);         
+        }
+		
+        waitpid(child2Wait, NULL,0);
+        
+        read(ring_pipes[start][0], &buffer, sizeof(int));
+		close(ring_pipes[start][0]);
+        printf("Soy el proceso PADRE, y el numero final es %d\n", buffer);
+
+        exit(0);
+    }
 }
