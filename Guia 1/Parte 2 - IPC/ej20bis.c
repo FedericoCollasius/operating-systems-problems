08a661b99aec97202e451b9cd340abba63d46a1a
Federico Collasius <fede.collasius@gmail.com>
2023-10-02 13:01:34 -0300

    Ej20 y Ej20bis

--- Guia 1/Parte 2 - IPC/ej20bis.c
+++ Guia 1/Parte 2 - IPC/ej20bis.c
@@ -17,30 +17,29 @@ void informarResultado(int numero, int resultado){
     printf("El numero sobre el que se ejecuto el calculo fue %d y el resultado %d \n", numero, resultado);
 }
 
-void ejecutarHijo (int i, int N, int pipes[][2]) {
+void ejecutarHijo (int i, int pipes[][2]) {
     int secreto;
     read(pipes[i][0], &secreto, sizeof(secreto));
+    close(pipes[i][0]);
     int internalPipe[2];
     pipe(internalPipe);
     int hijo_pid = getpid();
     int nieto_pid = fork();
     if(nieto_pid == 0){
+        close(internalPipe[0]);
         int res = calcular(secreto);
         write(internalPipe[1], &res, sizeof(res));
+        exit(0);
     } else {
+        close(internalPipe[1]);
         wait(NULL);
-        int res_nieto; 
+        int res_nieto;
         read(internalPipe[0], &res_nieto, sizeof(res_nieto));
-        int consulta_padre; 
-        read(pipes[i][0], &consulta_padre, sizeof(consulta_padre));
-        if(!consulta_padre){
-            consulta_padre = 1; 
-            write(pipes[N + i][1], &consulta_padre, sizeof(consulta_padre));
-            write(pipes[N + i][1], &secreto, sizeof(secreto));
-            write(pipes[N + i][1], &res_nieto, sizeof(res_nieto));
+        int termino = 1;
+        write(pipes[i][1], &termino, sizeof(termino));
+        write(pipes[i][1], &secreto, sizeof(secreto));
+        write(pipes[i][1], &res_nieto, sizeof(res_nieto));
         }
-    }
-    exit(0);
 }
 
 int main(int argc, char* argv[]){
@@ -50,8 +49,8 @@ int main(int argc, char* argv[]){
     }
 
     int N = atoi(argv[1]);
-    int pipes[N*2][2];
-    for (int i=0; i< N*2; i++){
+    int pipes[N][2];
+    for (int i=0; i< N; i++){
         pipe(pipes[i]); 
     }
     
@@ -59,11 +58,12 @@ int main(int argc, char* argv[]){
     for (int i=0; i< N; i++) {
         pid_t pid = fork();
         if (pid==0) {
-            ejecutarHijo(i, N, pipes);
+            ejecutarHijo(i,pipes);
             return 0;
         } else {
             int numero = dameNumero(pid);
             write(pipes[i][1], &numero, sizeof(numero)); 
+            close(pipes[i][1]);
         } 
     }
 
@@ -77,14 +77,13 @@ int main(int argc, char* argv[]){
             if (hijoTermino[i]) {
                 continue; 
             }
-            int termino = 0;
-            write(pipes[i][1], &termino, sizeof(termino));
-            read(pipes[N+i][0], &termino, sizeof(termino));
+            int termino;
+            read(pipes[i][0], &termino, sizeof(termino));
             if (termino){
                 int numero;
                 int resultado;
-                read(pipes[N+i][0], &numero, sizeof(numero));
-                read(pipes[N+i][0], &resultado, sizeof(resultado));
+                read(pipes[i][0], &numero, sizeof(numero));
+                read(pipes[i][0], &resultado, sizeof(resultado));
                 informarResultado(numero, resultado);
                 hijoTermino[i] = 1;
                 cantidadTerminados++; 
